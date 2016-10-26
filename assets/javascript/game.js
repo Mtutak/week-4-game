8ac65e7e498a20ec1a5258f26f9039606171c448
Matthew Tutak <mtutak91@gmail.com>
2016-10-25 22:43:25 -0500

    adding core files and structure

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	console.log('game');
+//Player Chooses Character With Click
+	$('.pick').click(function(){
+		$(this).toggleClass('chosen');
+		// $('.chosen').appendTo('.character');
+	});
+
+	//if statement to prevent multiple clicks
+
+var characterOne = {health:100, 
+					attack:100,
+					counter:100};
+var characterTwo = {health:100, 
+					attack:100,
+					counter:100};
+var characterThree = {health:100, 
+					attack:100,
+					counter:100};
+
+});
+
