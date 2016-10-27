0569fb073918e371dd003288888812796ab7a4f9
Matthew Tutak <mtutak91@gmail.com>
2016-10-27 18:19:49 -0500

    Moving Blocks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,47 @@
+var clicks = 0;
+var fight = 0;
+var attack = 0;
 $(document).ready(function () {
 	console.log('game');
 //Player Chooses Character With Click
 	$('.pick').click(function(){
-		$(this).toggleClass('chosen');
+		clicks++;
+		console.log('clicks' +clicks);
+		if(clicks<2) {
+		$(this).addClass('chosen').appendTo('.characterRow').removeClass('pick'); }
 		// $('.chosen').appendTo('.character');
+		if ($('.characterRow').has('.chosen')) {
+		$('.pick').addClass('enemies').appendTo('.enemyRow').removeClass('pick');
+			$('.enemies').click(function(){
+		fight++;
+		console.log('fight' + fight);
+		$(this).addClass('defender').appendTo('.defenderRow');
 	});
+		}
+	});
+	
+$('#attack').click(function(){
+	attack++
+	console.log('attack ' + attack);
 
-	//if statement to prevent multiple clicks
+});
 
-var characterOne = {health:100, 
-					attack:100,
-					counter:100};
-var characterTwo = {health:100, 
-					attack:100,
-					counter:100};
-var characterThree = {health:100, 
-					attack:100,
-					counter:100};
+var characterOne = {
+	health:100, 
+	attack:100,
+	counter:100
+};
+var characterTwo = {
+	health:100, 
+	attack:100,
+	counter:100
+};
+var characterThree = {
+	health:100, 
+	attack:100,
+	counter:100
+};
+
 
 });
 
